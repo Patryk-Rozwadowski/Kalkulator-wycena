4ba08711c085a5f204adf43038fa9ae30be51c98
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-04-02 15:08:36 +0200

    Add seo option scripts

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
 var clientValue = 100;
 var meValue = 200;
 
+//CENY ZA USŁUGI
+var graphicProject = 300;
+var tempProject = 100;
+
+var seoPrice = 3000;
+var copyWritingPrice = 500;
+var rwdPrice = 1000;
+
 // WARTOŚC SUWAKA
 var sliderValue = document.getElementById('sliderValue');
 var sliderOutput = document.getElementById('value');
@@ -12,17 +20,51 @@ var forMeOutput = document.getElementById('forMeOutput');
 
 // WYPISANIE WARTOŚCI SUWAKA
 sliderValue.addEventListener('input', function() {
+
   sliderOutput.innerHTML = sliderValue.value;
 
-  // JEŚLI KLIENT WYBRAL PROJEKT GRAFICZNY TO a*b
+  clientOptionProject();
+  clientOptionSEO();
+});
+
+// WYBÓR PROJEKT GRAFICZNY / Szablon
+
+function clientOptionProject(subpageCount) {
+  var subpageCount = sliderValue.value;
+
+  // JEŚLI KLIENT WYBRAL PROJEKT GRAFICZNY
   if (document.getElementById('clientChooseProject').value == "Graphic"){
-    forClientOutput.innerHTML = sliderValue.value * clientValue;
-    forMeOutput.innerHTML = sliderValue.value * meValue;
+    forClientOutput.innerHTML = subpageCount * graphicProject;
+    forMeOutput.innerHTML = subpageCount * graphicProject;
+
+    document.getElementById('ProjectPrice').innerHTML = graphicProject + 'zł';
   }
 
-  // JEŚLI NIE WYBRAŁ PROJEKTU GRAFICZNEGO TO
-  else {
-    forClientOutput.innerHTML = sliderValue.value * 1000;
-    forMeOutput.innerHTML = sliderValue.value * 2000;
+  // JEŚLI NIE WYBRAŁ PROJEKTU GRAFICZNEGO TO WYBRAŁ SZABLON
+  else if (document.getElementById('clientChooseProject').value == "Temp"){
+    forClientOutput.innerHTML = subpageCount * tempProject;
+    forMeOutput.innerHTML = subpageCount * tempProject;
+
+    document.getElementById('ProjectPrice').innerHTML = tempProject + 'zł';
   }
-});
+  return subpageCount;
+};
+
+// WYBÓR SEO
+function clientOptionSEO(subpageCount) {
+  var subpageCount = sliderValue.value;
+
+  // JEŚLI KLIENT WYBRAL SEO
+  if (document.getElementById('clientChooseSEO').value == "yes"){
+    forClientOutput.innerHTML = subpageCount * seoPrice;
+    forMeOutput.innerHTML = subpageCount * seoPrice;
+
+    document.getElementById('SEOPrice').innerHTML = seoPrice + 'zł';
+  }
+
+  // JEŚLI NIE WYBRAŁ SEO
+  else if (document.getElementById('clientChooseSEO').value == "no"){
+    document.getElementById('SEOPrice').innerHTML =  '0 zł';
+  }
+  return subpageCount;
+}
