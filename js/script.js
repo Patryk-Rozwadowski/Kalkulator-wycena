72bb0429c678baae92043030fcc8a6946d47f8c9
Patryk Rozwadowski <rozwadowski44@o2.pl>
2019-04-02 17:36:32 +0200

    Last version

--- js/script.js
+++ js/script.js
@@ -17,54 +17,86 @@ var sliderOutput = document.getElementById('value');
 var forClientOutput = document.getElementById('forClientOutput');
 var forMeOutput = document.getElementById('forMeOutput');
 
-
 // WYPISANIE WARTOŚCI SUWAKA
-sliderValue.addEventListener('input', function() {
+sliderValue.addEventListener('input', function(seoPrice, graphicProject, tempProject) {
 
   sliderOutput.innerHTML = sliderValue.value;
 
   clientOptionProject();
   clientOptionSEO();
+  clientOptionCW();
 });
 
 // WYBÓR PROJEKT GRAFICZNY / Szablon
 
-function clientOptionProject(subpageCount) {
-  var subpageCount = sliderValue.value;
+function clientOptionProject() {
+
+
+  let projectPrice = graphicProject * sliderValue.value ;
+  let tempProjectPrice = tempProject * sliderValue.value;
 
   // JEŚLI KLIENT WYBRAL PROJEKT GRAFICZNY
   if (document.getElementById('clientChooseProject').value == "Graphic"){
-    forClientOutput.innerHTML = subpageCount * graphicProject;
-    forMeOutput.innerHTML = subpageCount * graphicProject;
 
+    forClientOutput.innerHTML = projectPrice;
     document.getElementById('ProjectPrice').innerHTML = graphicProject + 'zł';
   }
 
   // JEŚLI NIE WYBRAŁ PROJEKTU GRAFICZNEGO TO WYBRAŁ SZABLON
   else if (document.getElementById('clientChooseProject').value == "Temp"){
-    forClientOutput.innerHTML = subpageCount * tempProject;
-    forMeOutput.innerHTML = subpageCount * tempProject;
 
+    forClientOutput.innerHTML = tempProjectPrice ;
     document.getElementById('ProjectPrice').innerHTML = tempProject + 'zł';
   }
-  return subpageCount;
 };
 
 // WYBÓR SEO
-function clientOptionSEO(subpageCount) {
-  var subpageCount = sliderValue.value;
+function clientOptionSEO() {
 
   // JEŚLI KLIENT WYBRAL SEO
   if (document.getElementById('clientChooseSEO').value == "yes"){
-    forClientOutput.innerHTML = subpageCount * seoPrice;
-    forMeOutput.innerHTML = subpageCount * seoPrice;
 
+    forClientOutput.innerHTML = seoPrice * sliderValue.value;
     document.getElementById('SEOPrice').innerHTML = seoPrice + 'zł';
   }
 
   // JEŚLI NIE WYBRAŁ SEO
   else if (document.getElementById('clientChooseSEO').value == "no"){
+
     document.getElementById('SEOPrice').innerHTML =  '0 zł';
   }
-  return subpageCount;
+}
+
+// WYBÓR COPYWRITING
+function clientOptionCW() {
+
+  // JEŚLI KLIENT WYBRAL COPYWRITING
+  if (document.getElementById('clientCW').value == "yes"){
+
+    forClientOutput.innerHTML = copyWritingPrice * sliderValue.value;
+    document.getElementById('CWPrice').innerHTML = copyWritingPrice + 'zł';
+  }
+
+  // JEŚLI NIE WYBRAŁ COPYWRITING
+  else if (document.getElementById('clientCW').value == "no"){
+
+    document.getElementById('CWPrice').innerHTML =  '0 zł';
+  }
+}
+
+// WYBÓR RWD
+function clientOptionRWD() {
+
+  // JEŚLI KLIENT WYBRAL RWD
+  if (document.getElementById('clientChooseRWD').value == "yes"){
+
+    forClientOutput.innerHTML = rwdPrice * sliderValue.value;
+    document.getElementById('RWDPrice').innerHTML = rwdPrice + 'zł';
+  }
+
+  // JEŚLI NIE WYBRAŁ RWD
+  else if (document.getElementById('clientChooseRWD').value == "no"){
+
+    document.getElementById('RWDPrice').innerHTML =  '0 zł';
+  }
 }
